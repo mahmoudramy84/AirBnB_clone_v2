5280008bed2deb1692b4cd28aa18fab33922281d
mahmoudramy84 <mahmoudramy84@gmail.com>
2024-01-17 08:01:38 +0200

    adding setup_mysql_test.sql script file to solve task4

---
+++ setup_mysql_test.sql
@@ -0,0 +1,13 @@
+-- setup_mysql_test.sql
+
+-- Create the database if it doesn't exist
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+
+-- Create the user if it doesn't exist
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+
+-- Grant SELECT privilege on the performance_schema database
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
+
+-- Grant all privileges on the hbnb_test_db database
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
